843143cef9344d131255c8700576452df6efc4af
Chris Barnes <barnesc@janelia.hhmi.org>
2020-04-15 19:33:17 +0100

    WIP PyO3: round out methods

--- nblast-py/src/lib.rs
+++ nblast-py/src/lib.rs
@@ -2,12 +2,14 @@ use pyo3::exceptions;
 use pyo3::prelude::*;
 use std::collections::HashMap;
 
-use nblast::{table_to_fn, DistDot, DotPropIdx, NblastArena, Precision};
+use nblast::{
+    table_to_fn, DistDot, NblastArena, NeuronIdx, Precision, RStarPointTangents,
+};
 
 #[pyclass]
 pub struct ArenaWrapper {
     // TODO: can this box be avoided?
-    arena: NblastArena<Box<dyn Fn(&DistDot) -> Precision>>,
+    arena: NblastArena<RStarPointTangents, Box<dyn Fn(&DistDot) -> Precision>>,
 }
 
 fn vec_to_array3<T: Sized + Copy>(v: &Vec<T>) -> [T; 3] {
@@ -31,47 +33,58 @@ impl ArenaWrapper {
 
     fn add_points(&mut self, _py: Python, points: Vec<Vec<f64>>) -> PyResult<usize> {
         // TODO: avoid this copy?
-        let v: Vec<[f64; 3]> = points.iter().map(vec_to_array3).collect();
-        self.arena
-            .add_points(&v)
-            .map_err(|s| PyErr::new::<exceptions::RuntimeError, _>(s))
+        let neuron = RStarPointTangents::new(points.iter().map(vec_to_array3).collect())
+            .map_err(|s| PyErr::new::<exceptions::RuntimeError, _>(s))?;
+        Ok(self.arena.add_neuron(neuron))
     }
 
     pub fn query_target(
         &self,
         _py: Python,
-        query_idx: DotPropIdx,
-        target_idx: DotPropIdx,
-        normalise: bool,
+        query_idx: NeuronIdx,
+        target_idx: NeuronIdx,
+        normalize: bool,
         symmetric: bool,
     ) -> Option<f64> {
         self.arena
-            .query_target(query_idx, target_idx, normalise, symmetric)
+            .query_target(query_idx, target_idx, normalize, symmetric)
     }
 
     pub fn queries_targets(
         &self,
         _py: Python,
-        query_idxs: Vec<DotPropIdx>,
-        target_idxs: Vec<DotPropIdx>,
-        normalise: bool,
+        query_idxs: Vec<NeuronIdx>,
+        target_idxs: Vec<NeuronIdx>,
+        normalize: bool,
         symmetric: bool,
-    ) -> Option<HashMap<(DotPropIdx, DotPropIdx), f64>> {
+    ) -> HashMap<(NeuronIdx, NeuronIdx), f64> {
         self.arena
-            .queries_targets(&query_idxs, &target_idxs, normalise, symmetric)
+            .queries_targets(&query_idxs, &target_idxs, normalize, symmetric)
     }
 
     pub fn all_v_all(
         &self,
         _py: Python,
-        normalise: bool,
+        normalize: bool,
         symmetric: bool,
-    ) -> Option<HashMap<(DotPropIdx, DotPropIdx), Precision>> {
-        self.arena.all_v_all(normalise, symmetric)
+    ) -> HashMap<(NeuronIdx, NeuronIdx), Precision> {
+        self.arena.all_v_all(normalize, symmetric)
     }
 
     pub fn len(&self, _py: Python) -> usize {
         self.arena.len()
+    }
+
+    pub fn self_hit(&self, _py: Python, idx: NeuronIdx) -> Option<Precision> {
+        self.arena.self_hit(idx)
+    }
+
+    pub fn points(&self, _py: Python, idx: NeuronIdx) -> Option<Vec<[Precision; 3]>> {
+        self.arena.points(idx)
+    }
+
+    pub fn tangents(&self, _py: Python, idx: NeuronIdx) -> Option<Vec<[Precision; 3]>> {
+        self.arena.tangents(idx).map(|vectors| vectors.map(|v| [*v.x, *v.y, *v.z]))  // TODO: fix this
     }
 }
 
