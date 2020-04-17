c09ed5c500e3192efcc6cfac9cea8e74dbbd05c7
Chris Barnes <barnesc@janelia.hhmi.org>
2020-04-17 19:14:59 +0100

    support different symmetries

--- nblast-py/src/lib.rs
+++ nblast-py/src/lib.rs
@@ -1,8 +1,10 @@
 use pyo3::exceptions;
 use pyo3::prelude::*;
 use std::collections::HashMap;
+use core::fmt::Debug;
 
-use nblast::{table_to_fn, DistDot, NblastArena, NeuronIdx, Precision, RStarPointTangents};
+use nblast::nalgebra::base::{Vector3, Unit};
+use nblast::{table_to_fn, DistDot, NblastArena, NeuronIdx, Precision, RStarPointTangents, Symmetry};
 
 #[pyclass]
 pub struct ArenaWrapper {
@@ -12,6 +14,21 @@ pub struct ArenaWrapper {
 
 fn vec_to_array3<T: Sized + Copy>(v: &Vec<T>) -> [T; 3] {
     [v[0], v[1], v[2]]
+}
+
+fn vec_to_unitvector3<T: 'static + Sized + Copy + PartialEq + Debug>(v: &Vec<T>) -> Unit<Vector3<T>> {
+    Unit::new_unchecked(Vector3::new(v[0], v[1], v[2]))
+}
+
+fn str_to_sym(s: &str) -> Result<Symmetry, ()> {
+    match s {
+        "arithmetic_mean" => Ok(Symmetry::ArithmeticMean),
+        "geometric_mean" => Ok(Symmetry::GeometricMean),
+        "harmonic_mean" => Ok(Symmetry::HarmonicMean),
+        "min" => Ok(Symmetry::Min),
+        "max" => Ok(Symmetry::Max),
+        _ => Err(()),
+    }
 }
 
 #[pymethods]
@@ -36,16 +53,29 @@ impl ArenaWrapper {
         Ok(self.arena.add_neuron(neuron))
     }
 
+    fn add_points_tangents(&mut self, _py: Python, points: Vec<Vec<f64>>, tangents: Vec<Vec<f64>>) -> PyResult<usize> {
+        let neuron = RStarPointTangents::new_with_tangents(
+            points.iter().map(vec_to_array3).collect(),
+            tangents.iter().map(vec_to_unitvector3).collect(),
+        ).map_err(|s| PyErr::new::<exceptions::RuntimeError, _>(s))?;
+        Ok(self.arena.add_neuron(neuron))
+    }
+
     pub fn query_target(
         &self,
         _py: Python,
         query_idx: NeuronIdx,
         target_idx: NeuronIdx,
         normalize: bool,
-        symmetric: bool,
-    ) -> Option<f64> {
-        self.arena
-            .query_target(query_idx, target_idx, normalize, symmetric)
+        symmetry: Option<&str>,
+    ) -> PyResult<Option<f64>> {
+        let sym = match symmetry {
+            Some(s) => Some(str_to_sym(s).map_err(|_| PyErr::new::<exceptions::ValueError, _>("Symmetry type not recognised"))?),
+            _ => None,
+        };
+        Ok(self.arena
+            .query_target(query_idx, target_idx, normalize, &sym)
+        )
     }
 
     pub fn queries_targets(
@@ -54,19 +84,27 @@ impl ArenaWrapper {
         query_idxs: Vec<NeuronIdx>,
         target_idxs: Vec<NeuronIdx>,
         normalize: bool,
-        symmetric: bool,
-    ) -> HashMap<(NeuronIdx, NeuronIdx), f64> {
-        self.arena
-            .queries_targets(&query_idxs, &target_idxs, normalize, symmetric)
+        symmetry: Option<&str>,
+    ) -> PyResult<HashMap<(NeuronIdx, NeuronIdx), f64>> {
+        let sym = match symmetry {
+            Some(s) => Some(str_to_sym(s).map_err(|_| PyErr::new::<exceptions::ValueError, _>("Symmetry type not recognised"))?),
+            _ => None,
+        };
+        Ok(self.arena
+            .queries_targets(&query_idxs, &target_idxs, normalize, &sym))
     }
 
     pub fn all_v_all(
         &self,
         _py: Python,
         normalize: bool,
-        symmetric: bool,
-    ) -> HashMap<(NeuronIdx, NeuronIdx), Precision> {
-        self.arena.all_v_all(normalize, symmetric)
+        symmetry: Option<&str>,
+    ) -> PyResult<HashMap<(NeuronIdx, NeuronIdx), Precision>> {
+        let sym = match symmetry {
+            Some(s) => Some(str_to_sym(s).map_err(|_| PyErr::new::<exceptions::ValueError, _>("Symmetry type not recognised"))?),
+            _ => None,
+        };
+        Ok(self.arena.all_v_all(normalize, &sym))
     }
 
     pub fn len(&self, _py: Python) -> usize {
