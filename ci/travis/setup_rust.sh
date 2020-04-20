30b3783acfdc8277998a4d0fee682de910d6e126
Chris Barnes <barnesc@janelia.hhmi.org>
2020-04-20 14:34:27 +0100

    fix typo in setup_rust

--- ci/travis/setup_rust.sh
+++ ci/travis/setup_rust.sh
@@ -4,7 +4,7 @@ set -ex
 
 ### Setup Rust toolchain #######################################################
 
-RUST_VERSION=${RUST_VERSION:$TRAVIS_RUST_VERSION}
+RUST_VERSION=${RUST_VERSION:-$TRAVIS_RUST_VERSION}
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$RUST_VERSION
 export PATH=$PATH:$HOME/.cargo/bin
