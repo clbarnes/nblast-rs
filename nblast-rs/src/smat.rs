c731b5ca3aad46790acdf376f0e55e7234f0e971
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-04-05 11:06:04 +0100

    Refactor smat creation logic into separate structs

--- nblast-rs/src/smat.rs
+++ nblast-rs/src/smat.rs
@@ -28,9 +28,11 @@ use std::collections::HashSet;
 use std::error;
 use std::fmt;
 use std::iter;
+use std::ops::Range;
 #[cfg(feature = "rayon")]
 use std::sync::mpsc::channel;
 
+use crate::Neuron;
 use crate::{BinLookup, NdBinLookup, RangeTable};
 use crate::{DistDot, Precision, TargetNeuron};
 
@@ -72,12 +74,12 @@ fn logspace(
 struct PairSampler {
     sets: Vec<Vec<usize>>,
     cumu_weights: Vec<f64>,
-    rng: fastrand::Rng,
+    pub rng: fastrand::Rng,
 }
 
 impl PairSampler {
     fn new(sets: Vec<Vec<usize>>, seed: Option<u64>) -> Self {
-        let mut rng = fastrand::Rng::new();
+        let rng = fastrand::Rng::new();
         if let Some(s) = seed {
             rng.seed(s);
         }
@@ -88,10 +90,14 @@ impl PairSampler {
             acc += *x / total;
             *x = acc;
         }
-        Self { sets, cumu_weights: weights, rng }
+        Self {
+            sets,
+            cumu_weights: weights,
+            rng,
+        }
     }
 
-    pub fn new_from_sets(sets: &[HashSet<usize>], seed: Option<u64>) -> Self {
+    pub fn from_sets(sets: &[HashSet<usize>], seed: Option<u64>) -> Self {
         let vecs = sets
             .iter()
             .map(|s| {
@@ -111,14 +117,18 @@ impl PairSampler {
             return 0;
         }
         let rand = self.rng.f64();
-        match self.cumu_weights.binary_search_by(|w| w.partial_cmp(&rand).expect("NaN weight")) {
+        match self
+            .cumu_weights
+            .binary_search_by(|w| w.partial_cmp(&rand).expect("NaN weight"))
+        {
             Ok(idx) => idx,
             Err(idx) => idx,
         }
     }
 
     pub fn sample(&mut self) -> (usize, usize) {
-        let set = self.sets[self.outer_idx()];
+        let set_idx = self.outer_idx();
+        let set = &self.sets[set_idx];
         let first = self.rng.usize(0..set.len());
         let mut second = first;
         while second == first {
@@ -127,12 +137,53 @@ impl PairSampler {
         (first, second)
     }
 
-    pub fn sample_n(&mut self, n: usize) -> Vec<(usize, usize)> {
-        std::iter::repeat_with(|| self.sample()).take(n).collect()
+    /// If not enough unique pairs can be found, the Err response contains all pairs, but fewer than requested.
+    pub fn sample_n(
+        &mut self,
+        n: usize,
+    ) -> Result<HashSet<(usize, usize)>, HashSet<(usize, usize)>> {
+        let mut out = HashSet::default();
+
+        // If n is not much less than the total number of pairs,
+        // easier to enumerate all pairs and shuffle from them.
+        if n * 10 < self.n_pairs() {
+            let mut count = 0;
+            while out.len() < n {
+                out.insert(self.sample());
+                count += 1;
+                if count >= n * 10 {
+                    return Err(out);
+                }
+            }
+        } else {
+            let mut v = self.exhaust();
+            self.rng.shuffle(&mut v);
+
+            while out.len() < n {
+                if let Some(s) = v.pop() {
+                    out.insert(s);
+                } else {
+                    return Err(out);
+                }
+            }
+        }
+        Ok(out)
     }
 
-    pub fn len(&self) -> usize {
-        self.sets.iter().map(|v| v.len().pow(2)).sum()
+    /// Upper bound on the number of pairs.
+    /// If some pair sets intersect, there could be fewer unique pairs.
+    pub fn n_pairs(&self) -> usize {
+        self.sets
+            .iter()
+            .map(|v| {
+                let len = v.len();
+                if len < 2 {
+                    0
+                } else {
+                    len * (len - 1)
+                }
+            })
+            .sum()
     }
 
     pub fn n_sets(&self) -> usize {
@@ -154,6 +205,183 @@ impl PairSampler {
 
         out
     }
+}
+
+pub struct TrainingSampler {
+    rng: fastrand::Rng,
+    matching_sets: Vec<HashSet<usize>>,
+    nonmatching_sets: Option<Vec<HashSet<usize>>>,
+    n_neurons: usize,
+}
+
+impl TrainingSampler {
+    pub fn new(n_neurons: usize, seed: Option<u64>) -> Self {
+        let rng = fastrand::Rng::new();
+        if let Some(s) = seed {
+            rng.seed(s);
+        }
+        Self {
+            rng,
+            matching_sets: Vec::default(),
+            nonmatching_sets: None,
+            n_neurons,
+        }
+    }
+
+    /// Add a set of neurons which are considered to be mutually matching,
+    /// as indices into a vec of neurons of length `self.n_neurons`.
+    /// This can be done several times with different sets.
+    /// Matching sets should be small subsets of the total neuron count.
+    ///
+    /// Indices outside of the neuron vec will be silently filtered out.
+    /// Filtered sets with length <2 will also be silently ignored.
+    pub fn add_matching_set(&mut self, matching: &[usize]) -> &mut Self {
+        // ensure all neurons exist
+        let set: HashSet<usize> = matching
+            .iter()
+            .filter_map(|idx| {
+                if idx < &self.n_neurons {
+                    Some(*idx)
+                } else {
+                    None
+                }
+            })
+            .collect();
+        if set.len() >= 2 {
+            self.matching_sets.push(set);
+        }
+        self
+    }
+
+    /// Optionally list neuron sets which are mutually non-matching,
+    /// as indices into a vec of neurons of length `self.n_neurons`,
+    /// from which non-matching pairs will be randomly drawn.
+    /// By default, all neurons are included,
+    /// as matching sets are expected to be only a small portion of the total neurons.
+    ///
+    /// Candidate non-matching pairs are made sure not to be in the matching pairs,
+    /// so this can safely be ignored.
+    pub fn add_nonmatching_set(&mut self, nonmatching: &[usize]) -> &mut Self {
+        let set: HashSet<usize> = nonmatching
+            .iter()
+            .filter_map(|idx| {
+                if idx < &self.n_neurons {
+                    Some(*idx)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        if set.len() < 2 {
+            return self;
+        }
+
+        if let Some(ref mut vector) = self.nonmatching_sets {
+            vector.push(set);
+        } else {
+            let mut v = Vec::default();
+            v.push(set);
+            self.nonmatching_sets = Some(v);
+        }
+        self
+    }
+
+    /// Generate the "jobs" for matching and nonmatching neuron queries.
+    ///
+    /// If n_matching is `None`, uses all unique ordered matching pairs.
+    /// If n_nonmatching is `None`, uses the same number as there are matching pairs.
+    ///
+    /// The first element of the tuple is the matching jobs; the second set is non-matching.
+    pub fn make_jobs(
+        &self,
+        n_matching: Option<usize>,
+        n_nonmatching: Option<usize>,
+    ) -> (JobSet, JobSet) {
+        let mut match_sampler =
+            PairSampler::from_sets(&self.matching_sets, Some(self.rng.u64(0..u64::MAX)));
+
+        let matching_jobs: JobSet = match n_matching {
+            Some(n) => match match_sampler.sample_n(n) {
+                Ok(jobs) => jobs,
+                Err(jobs) => jobs,
+            },
+            None => match_sampler.exhaust().into_iter().collect(),
+        };
+
+        // if nonmatching not given, use all neurons
+        let nonmatch_seed = Some(self.rng.u64(0..u64::MAX));
+        let mut nonmatch_sampler = self.nonmatching_sets.as_ref().map_or_else(
+            || {
+                let v = vec![(0..self.n_neurons).collect()];
+                PairSampler::from_sets(&v, nonmatch_seed)
+            },
+            |s| PairSampler::from_sets(&s, nonmatch_seed),
+        );
+
+        let n_nm = n_nonmatching.unwrap_or(matching_jobs.len());
+
+        if n_nm > nonmatch_sampler.n_pairs() {
+            panic!("Not enough non-matching neurons")
+        }
+
+        let nonmatching_jobs = nonmatch_sampler.sample_n(n_nm).unwrap_or_else(|s| s);
+
+        (matching_jobs, nonmatching_jobs)
+    }
+}
+
+fn all_distdots<T: TargetNeuron>(
+    neurons: &[T],
+    jobs: &[(usize, usize)],
+    use_alpha: bool,
+) -> Vec<DistDot> {
+    jobs.iter()
+        .map(|(q, t)| neurons[*q].query_dist_dots(&neurons[*t], use_alpha))
+        .flatten()
+        .collect()
+}
+
+/// Uses global thread pool by default. To use your own threadpool, use
+///
+/// ```rust
+/// let pool = rayon::ThreadPoolBuilder::new()
+///     .num_threads(n_threads)
+///     .build()
+///     .unwrap();
+/// let results = pool.install(|| all_distdots_par(&neurons, &jobs, use_alpha));
+/// ```
+#[cfg(feature = "parallel")]
+pub fn all_distdots_par<T: TargetNeuron + Sync>(
+    neurons: &[T],
+    jobs: &[(usize, usize)],
+    use_alpha: bool,
+) -> Vec<DistDot> {
+    jobs.par_iter()
+        .map(|(q, t)| neurons[*q].query_dist_dots(&neurons[*t], use_alpha))
+        .flatten()
+        .collect()
+}
+
+/// Find indices into a flattened array of `DistDot`s (as returned by `all_distdots` et al.)
+/// which correspond to particular jobs.
+/// i.e. element `i` of the output will contain a range which corresponds to the job at `jobs[i]`.
+/// If the same neurons and jobs were given to `all_distdots`, that range could index the output
+/// to find the distdots corresponding to that job.
+pub fn job_idxs<T: Neuron>(neurons: &[T], jobs: &[(usize, usize)]) -> Vec<Range<usize>> {
+    let mut start = 0;
+    let mut out = Vec::with_capacity(jobs.len());
+    for (q, _) in jobs.iter() {
+        let len = neurons[*q].len();
+        out.push(start..start + len);
+        start += len;
+    }
+    out
+}
+
+enum LookupArgs {
+    Lookup(BinLookup<Precision>),
+    NBins(usize),
 }
 
 /// Calculate a score matrix (lookup table for converting point matches
@@ -327,65 +555,28 @@ impl<T: TargetNeuron + Sync> ScoreMatrixBuilder<T> {
     /// (if `None`, use all neurons) until there are at least as many non-matching DistDots
     /// as there are matching.
     fn _match_nonmatch_jobs(&self) -> (JobSet, JobSet) {
-        let mut matching_len = 0;
-        let mut matching_jobs = JobSet::default();
+        let rng = fastrand::Rng::with_seed(self.seed);
 
-        // for every matching set
-        for matching_set in self.matching_sets.iter() {
-            // for every possible query idx
-            for q_idx in matching_set.iter() {
-                // if the idx is in the given neurons
-                let q_len = match self.neurons.get(*q_idx) {
-                    Some(n) => n.len(),
-                    None => continue,
-                };
-                // for every possible target idx
-                for t_idx in matching_set.iter() {
-                    // if t!=q, t exists, and the pair is not already addressed
-                    if t_idx != q_idx
-                        && t_idx < &self.neurons.len()
-                        && matching_jobs.insert((*q_idx, *t_idx))
-                    {
-                        // keep track of how many distdots we're producing
-                        matching_len += q_len
-                    }
-                }
-            }
-        }
+        let match_sampler = PairSampler::from_sets(&self.matching_sets, Some(rng.u64(0..u64::MAX)));
+        let matching_jobs: JobSet = match_sampler.exhaust().into_iter().collect();
 
         // if nonmatching not given, use all neurons
-        let nonmatching_sets = self
-            .nonmatching_sets
-            .unwrap_or_else(|| vec![(0..self.neurons.len()).collect()]);
+        let nonmatch_seed = Some(rng.u64(0..u64::MAX));
+        let mut nonmatch_sampler = self.nonmatching_sets.as_ref().map_or_else(
+            || {
+                let v = vec![(0..self.neurons.len()).collect()];
+                PairSampler::from_sets(&v, nonmatch_seed)
+            },
+            |s| PairSampler::from_sets(&s, nonmatch_seed),
+        );
 
-        let nonmatching_idxs = self
-            .nonmatching_sets
-            .as_ref()
-            .cloned() // ? is this avoidable
-            .or_else(|| Some(vec![(0..self.neurons.len()).collect()]))
-            .unwrap();
-
-        if matching_jobs.len() > nonmatching_idxs.len() * (nonmatching_idxs.len() - 1) {
+        if matching_jobs.len() > nonmatch_sampler.n_pairs() {
             panic!("Not enough non-matching neurons")
         }
 
-        let idx_range = ..nonmatching_idxs.len();
-        let rng = fastrand::Rng::new();
-        rng.seed(self.seed);
-
-        let mut nonmatching_jobs: HashSet<(usize, usize)> = HashSet::default();
-
-        // randomly pick nonmatching pairs until we have requested as many distdots
-        // as we did for matching
-        while matching_len > 0 {
-            let q_idx = nonmatching_idxs[rng.usize(idx_range)];
-            let t_idx = nonmatching_idxs[rng.usize(idx_range)];
-
-            let key = (q_idx, t_idx);
-            if q_idx != t_idx && !matching_jobs.contains(&key) && nonmatching_jobs.insert(key) {
-                matching_len -= self.neurons[q_idx].len()
-            }
-        }
+        let nonmatching_jobs = nonmatch_sampler
+            .sample_n(matching_jobs.len())
+            .unwrap_or_else(|s| s);
 
         (matching_jobs, nonmatching_jobs)
     }
